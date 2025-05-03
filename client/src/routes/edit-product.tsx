c62f9400899f18fae57f03675fd3d153de1245c0
Arminas Brazenas <arminas.brazenas@gmail.com>
2025-05-03 15:31:05 +0300

    Edit title

--- client/src/routes/edit-product.tsx
+++ client/src/routes/edit-product.tsx
@@ -2,8 +2,10 @@ import { Container } from "@mantine/core";
 import { Navigate, useParams } from "react-router-dom";
 import { EditProduct } from "../features/products/components/edit-product";
 import { paths } from "../shared/config/paths";
+import { usePageTitle } from "../shared/hooks/use-page-title";
 
 export const EditProductRoute = () => {
+  usePageTitle({ title: "Edit product" });
   const { productId } = useParams();
 
   if (!productId) {
